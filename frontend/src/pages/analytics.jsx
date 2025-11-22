4821616ee44bc6e08d2029b23d42887cac4b6f04
Sahil Gupta <sahilgupta70500@gmail.com>
2025-11-22 18:02:16 -0500

    updated

--- frontend/src/pages/analytics.jsx
+++ frontend/src/pages/analytics.jsx
@@ -1,11 +1,16 @@
+// analytics.jsx
 export const pageview = (url) => {
-  window.gtag('config', 'G-YQ1YLLT788', { page_path: url });
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', 'page_view', { page_path: url });
+  }
 };
 
 export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
